6af137f97c0e44903feb96a8b4ff48bc1f322b93
NSEcho <xdaemonx@protonmail.ch>
2025-06-01 14:24:07 +0200

    update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /* jshint esnext: true, evil: true */
 
 import {getApi, defaultInvocationOptions} from './lib/api.js';
+import { makeMethodInvocationWrapper } from './lib/invocationwrapper.js';
 import * as fastpaths from './lib/fastpaths.js';
 
 function Runtime() {
@@ -19,7 +20,6 @@ function Runtime() {
     let readObjectIsa = null;
     const msgSendBySignatureId = new Map();
     const msgSendSuperBySignatureId = new Map();
-    let markUsed = null;
     let cachedNSString = null;
     let cachedNSStringCtor = null;
     let cachedNSNumber = null;
@@ -1757,131 +1757,6 @@ function Runtime() {
         }
 
         callbacks.onComplete();
-    }
-
-    function makeMethodInvocationWrapper(method, owner, superSpecifier, invocationOptions) {
-        const sel = method.sel;
-        let handle = method.handle;
-        let types;
-        if (handle === undefined) {
-            handle = null;
-            types = method.types;
-        } else {
-            types = api.method_getTypeEncoding(handle).readUtf8String();
-        }
-
-        const signature = parseSignature(types);
-        const retType = signature.retType;
-        const argTypes = signature.argTypes.slice(2);
-
-        const objc_msgSend = superSpecifier
-            ? getMsgSendSuperImpl(signature, invocationOptions)
-            : getMsgSendImpl(signature, invocationOptions);
-
-        // hack to prevent rollup from dropping objc_msgSend
-        markUsed = objc_msgSend.add("0x0");
-        
-        const argVariableNames = argTypes.map(function (t, i) {
-            return "a" + (i + 1);
-        });
-        const callArgs = [
-            superSpecifier ? "superSpecifier" : "this",
-            "sel"
-        ].concat(argTypes.map(function (t, i) {
-            if (t.toNative) {
-                return "argTypes[" + i + "].toNative.call(this, " + argVariableNames[i] + ")";
-            }
-            return argVariableNames[i];
-        }));
-        let returnCaptureLeft;
-        let returnCaptureRight;
-        if (retType.type === 'void') {
-            returnCaptureLeft = "";
-            returnCaptureRight = "";
-        } else if (retType.fromNative) {
-            returnCaptureLeft = "return retType.fromNative.call(this, ";
-            returnCaptureRight = ")";
-        } else {
-            returnCaptureLeft = "return ";
-            returnCaptureRight = "";
-        }
-
-        const m = eval("var m = function (" + argVariableNames.join(", ") + ") { " +
-            returnCaptureLeft + "objc_msgSend(" + callArgs.join(", ") + ")" + returnCaptureRight + ";" +
-        " }; m;");
-
-        Object.defineProperty(m, 'handle', {
-            enumerable: true,
-            get: getMethodHandle
-        });
-
-        m.selector = sel;
-
-        Object.defineProperty(m, 'implementation', {
-            enumerable: true,
-            get() {
-                const h = getMethodHandle();
-
-                const impl = new NativeFunction(api.method_getImplementation(h), m.returnType, m.argumentTypes, invocationOptions);
-
-                const newImp = getReplacementMethodImplementation(h);
-                if (newImp !== null)
-                    impl._callback = newImp;
-
-                return impl;
-            },
-            set(imp) {
-                replaceMethodImplementation(getMethodHandle(), imp);
-            }
-        });
-
-        m.returnType = retType.type;
-
-        m.argumentTypes = signature.argTypes.map(t => t.type);
-
-        m.types = types;
-
-        Object.defineProperty(m, 'symbol', {
-            enumerable: true,
-            get() {
-                return `${method.kind}[${owner.$className} ${selectorAsString(sel)}]`;
-            }
-        });
-
-        m.clone = function (options) {
-            return makeMethodInvocationWrapper(method, owner, superSpecifier, options);
-        };
-
-        function getMethodHandle() {
-            if (handle === null) {
-                if (owner.$kind === 'instance') {
-                    let cur = owner;
-                    do {
-                        if ("- forwardingTargetForSelector:" in cur) {
-                            const target = cur.forwardingTargetForSelector_(sel);
-                            if (target === null)
-                                break;
-                            if (target.$kind !== 'instance')
-                                break;
-                            const h = api.class_getInstanceMethod(target.$class.handle, sel);
-                            if (!h.isNull())
-                                handle = h;
-                            else
-                                cur = target;
-                        } else {
-                            break;
-                        }
-                    } while (handle === null);
-                }
-
-                if (handle === null)
-                    throw new Error("Unable to find method handle of proxied function");
-            }
-
-            return handle;
-        }
-
-        return m;
     }
 
     function makeMethodImplementationWrapper(signature, implementation) {
